f574fe7bc8b01f7bc38f32fcf119f35a95d96955
gustavoleriam <129981063+gustavoleriam@users.noreply.github.com>
2024-02-13 13:45:58 -0300

    Primeiro commit

---
+++ src/components/Button.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./Button.css"
+
+const Button = ({id,text,action}) => {
+  const handleAction = (e) => {
+    action(e)
+  }
+  return (
+    <div>
+      <button id={id} onClick={handleAction} >{text}</button>
+    </div>
+  )
+}
+
+export default Button
